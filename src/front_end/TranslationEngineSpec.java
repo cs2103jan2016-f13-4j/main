d60b85fadf552a372992b91a05e128c3511d34bf
Thenaesh Elango <thenaeshelango@gmail.com>
2016-03-06 00:42:56 +0800

    add Translation Engine docs

--- src/front_end/TranslationEngineSpec.java
+++ src/front_end/TranslationEngineSpec.java
@@ -4,6 +4,20 @@ import java.util.List;
 import objects.*;
 
 
+/**
+ * The Translation Engine has the duty of
+ *      1) Getting user commands from the UI upon the order of the Dispatcher,
+ *              properly formatted as a Command object
+ *      2) Instructing the UI to display the results (of executing a command) sent to it,
+ *              in an appropriate manner
+ * 
+ * It has knowledge of the Command Parser and delegates to it to parse the user input string.
+ * 
+ * Rank: Captain
+ * 
+ * created by thenaesh on Mar 6, 2016
+ *
+ */
 public abstract class TranslationEngineSpec {
     void display(List<Task> taskList) {
     }
