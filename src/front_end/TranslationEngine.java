a0cfde562353fca25b2bd1dd69ba02e5c2dd154a
Thenaesh Elango <thenaeshelango@gmail.com>
2016-03-05 23:13:22 +0800

    add TranslationEngine spec

---
+++ src/front_end/TranslationEngine.java
@@ -0,0 +1,14 @@
+package front_end;
+
+import java.util.List;
+import objects.*;
+
+
+public class TranslationEngine {
+    void display(List<Task> taskList) {
+    }
+    
+    Command getCommand() {
+        return null;
+    }
+}
