ee8a7ff65574393fb6b7cacef3a46d48ece28a07
Thenaesh Elango <thenaeshelango@gmail.com>
2016-03-06 00:42:42 +0800

    add Command Parser docs

--- src/front_end/CommandParserSpec.java
+++ src/front_end/CommandParserSpec.java
@@ -3,6 +3,14 @@ package front_end;
 import objects.*;
 
 
+/**
+ * The Command Parser has one job: take a user input string and turn it into a Command object.
+ * 
+ * Rank: Lieutenant
+ * 
+ * created by thenaesh on Mar 6, 2016
+ *
+ */
 public abstract class CommandParserSpec {
     Command parseCommand(String cmdStr) {
         return null;
