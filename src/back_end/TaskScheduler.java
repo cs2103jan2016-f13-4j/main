2de174efe3cae8ab52b724d0663952103736da90
Thenaesh Elango <thenaeshelango@gmail.com>
2016-03-05 21:35:13 +0800

    add TaskScheduler spec

---
+++ src/back_end/TaskScheduler.java
@@ -0,0 +1,17 @@
+package back_end;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import objects.*;
+
+
+/**
+ * 
+ * created by thenaesh on Mar 5, 2016
+ *
+ */
+public class TaskScheduler {
+    public LocalDateTime findSlot(List<Task> existingTasks, long duration) {
+        return null;
+    }
+}
