71517584f68abe2dc93e1edc5d589a478bf422e3
Thenaesh Elango <thenaeshelango@gmail.com>
2016-03-05 23:38:39 +0800

    document Dispatcher and its primary heartbeat function

--- src/application/Dispatcher.java
+++ src/application/Dispatcher.java
@@ -5,6 +5,9 @@ import front_end.*;
 
 
 /**
+ * This class is the master controller that drives the (independent) front-end and back-end.
+ * Its function is analogous to that of the human heart with respect to pulmonary and systemic circulation.
+ * 
  * created by thenaesh on 5/3/16
  *
  */
@@ -12,6 +15,12 @@ public class Dispatcher {
     private DecisionEngineSpec decisionEngine = null;
     private TranslationEngineSpec translationEngine = null;
     
+    /*
+     * 1) pulls a command in from the front-end
+     * 2) pushes it to the back-end for processing
+     * 3) receives the resulting task list and other instructions from the back-end
+     * 4) pushes the instructions to the front end
+     */
     public void heartbeat() {
     }
 }
