b17647a5fb0440a7a741d57f553db46b99fceb51
Thenaesh Elango <thenaeshelango@gmail.com>
2016-03-06 00:24:43 +0800

    add Task Scheduler docs

--- src/back_end/TaskSchedulerSpec.java
+++ src/back_end/TaskSchedulerSpec.java
@@ -6,6 +6,14 @@ import objects.*;
 
 
 /**
+ * The Task Scheduler has one job: assist the Decision Engine with automatically finding a timeslot
+ * to place a task of specified duration.
+ * 
+ * This is typically done when the user specifies a task without sufficient information to explicitly determine
+ * a start and end time; in such situations, the task is considered floating and the Task Scheduler is called
+ * upon to find a slot.
+ * 
+ * Rank: Sergeant
  * 
  * created by thenaesh on Mar 5, 2016
  *
