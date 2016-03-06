0afa3b4fb7e30bbed40c4be90452e2302ba1d72e
Thenaesh Elango <thenaeshelango@gmail.com>
2016-03-06 11:06:18 +0800

    update docs for DataStore and UserInterface

--- src/front_end/ui/UserInterface.java
+++ src/front_end/ui/UserInterface.java
@@ -1,13 +1,37 @@
 package front_end.ui;
 
+import objects.*;
+
 
 /**
- * TODO: write docs
+ * The User Interface has two jobs:
+ *      1) render a view ({@link View})
+ *      2) get a user command string
+ * 
+ * The building of the view is the job of the Translation Engine; this class should not care about it.
+ * 
+ * Rank: Staff Sergeant (reports to CPT Translation Engine)
  * 
  * created by thenaesh on Mar 6, 2016
  *
  */
 public abstract class UserInterface {
-    public void render(){
+    
+    /**
+     * renders the supplied view
+     * 
+     * @param viewToRender
+     */
+    public void render(View viewToRender){
+    }
+    
+    /**
+     * gets an input string directly from the user
+     * Note that this method does not do any parsing; that is the job of the Command Parser
+     * 
+     * @return user input string
+     */
+    public String getUserInputString() {
+        return null;
     }
 }
