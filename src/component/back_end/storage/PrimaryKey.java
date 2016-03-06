5121bf17fd7b223d0275970c293bf57ebd0435cf
Mai Anh Vu <me@maianhvu.com>
2016-03-06 14:41:50 +0800

    add hashcode and equals behaviour to PrimaryKey

--- src/component/back_end/storage/PrimaryKey.java
+++ src/component/back_end/storage/PrimaryKey.java
@@ -29,4 +29,18 @@ public class PrimaryKey<T extends Comparable<T>> implements PrimaryKeySpec<T> {
         this.value_ = newKeyValue;
     }
 
+    @Override
+    public int hashCode() {
+        return this.getValue().hashCode();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null) return false;
+        if (this == o) return true;
+        if (!(o instanceof PrimaryKey)) return false;
+
+        PrimaryKey<T> key = (PrimaryKey<T>) o;
+        return this.getValue().equals(key.getValue());
+    }
 }
