d2840ac97990ba65719e66b700a8d37ae0278bbf
tio <tio.triatmoko@outlook.com>
2016-03-06 14:32:15 +0800

    add string constant to view.java and add helper methods to TaskListView.java

--- src/component/front_end/ui/View.java
+++ src/component/front_end/ui/View.java
@@ -1,24 +1,20 @@
 package component.front_end.ui;
 
 
-public class View<T> implements ViewSpec {
-    
+abstract class View<T> implements ViewSpec {
+    protected final static String STRING_NEW_LINE = "\n";
+    protected final static String STRING_TIME_CONNECTOR = "-";
+    protected final static String STRING_EMPTY = "";
     private T viewData_;
     
     public View(T data){
-        this.viewData_ = data; 
-
+        this.viewData_ = data;
     }
-    @Override
-    public String getContent() {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
+    
     @Override
     public Object getViewData() {
         // TODO Auto-generated method stub
         return this.viewData_;
     }
-
+    
 }
