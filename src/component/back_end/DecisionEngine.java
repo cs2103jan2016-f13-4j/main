3e4eef3026bb109dd3933ecab7467a213da8ed91
Thenaesh Elango <thenaeshelango@gmail.com>
2016-03-09 17:01:19 +0800

    add createTask method to DecisionEngine

--- src/component/back_end/DecisionEngine.java
+++ src/component/back_end/DecisionEngine.java
@@ -5,6 +5,7 @@ import component.front_end.*;
 import entity.*;
 import entity.command.*;
 
+import java.time.LocalDateTime;
 import java.util.*;
 
 
@@ -21,10 +22,55 @@ public class DecisionEngine extends DecisionEngineSpec {
         this.taskData_ = new TaskCollection();
     }
 
-    @Override
-    public Task createRawTask(Command command) {
+    
+    /**
+     * creates a Task from a specified command object when it makes sense
+     * we should blow up when creating a Task doesn't really make sense
+     * @param cmd
+     * @return
+     */
+    protected Task createTask(Command cmd) {
+        ParameterList params = cmd.getParameters();
         
-        switch (command.getInstruction().getType()) {
+        
+        // extract all the essential information out of the command
+        // the asserts ensure that we blow up if any error was made
+        // during the creation of the Command object in the Command Parser
+        ParameterValue nameRaw = params.getParameter(ParameterName.NAME);
+        assert (nameRaw.getValue() instanceof String);
+        String name = (String) nameRaw.getValue();
+        
+        ParameterValue fromRaw = params.getParameter(ParameterName.DATE_FROM);
+        assert (fromRaw.getValue() instanceof LocalDateTime);
+        LocalDateTime from = (LocalDateTime) fromRaw.getValue();
+        
+        ParameterValue toRaw = params.getParameter(ParameterName.DATE_TO);
+        assert (toRaw.getValue() instanceof LocalDateTime);
+        LocalDateTime to = (LocalDateTime) toRaw.getValue();
+        
+        
+        // we now build the Task object for adding into the store
+        Task taskToAdd = new Task(null, name, "", from, to);
+        
+        return taskToAdd;
+    }
+    
+    
+    @Override
+    public ExecutionResult<?> performCommand(Command cmd) {
+        
+        // this sort of nonsense should have been handled in the front end
+        assert (cmd.getInstruction().getType() != Instruction.Type.UNRECOGNISED);
+        
+        // handle exit command here, without creating a task unnecessarily
+        if (cmd.getInstruction().getType() == Instruction.Type.EXIT) {
+            return ExecutionResult.getNullResult();
+        }
+        
+        
+        // all the standard commands
+        // TODO: IMPLEMENT THESE!
+        switch (cmd.getInstruction().getType()) {
             case ADD:
                 // TODO
                 break;
@@ -37,33 +83,9 @@ public class DecisionEngine extends DecisionEngineSpec {
             case DELETE:
                 // TODO
                 break;
-            case UNRECOGNISED:
-                // TODO
-                break;
             default:
                 // if we reach this point, LTA Command Parser has failed in his duty
                 assert false;
-        }
-        return null;
-        
-    }
-    
-    protected Task createRawAddTask(Command cmd) {
-        ParameterList params = cmd.getParameters();
-        
-        // TODO: carry on
-        
-        return null;
-    }
-    
-    
-    @Override
-    public ExecutionResult<?> performCommand(Command cmd) {
-        
-        assert (cmd.getInstruction().getType() != Instruction.Type.UNRECOGNISED);
-        // handle exit command here, without creating a task unnecessarily
-        if (cmd.getInstruction().getType() == Instruction.Type.EXIT) {
-            return ExecutionResult.getNullResult();
         }
         
         return null;
--- test/component/back_end/DecisionEngineTest.java
+++ test/component/back_end/DecisionEngineTest.java
@@ -2,6 +2,7 @@ package component.back_end;
 
 import static org.junit.Assert.*;
 
+import java.time.Month;
 
 import org.junit.*;
 import component.back_end.storage.Task;
@@ -29,25 +30,34 @@ public class DecisionEngineTest {
      * TODO: fix it after the Command Parser has been fixed
      */
     @Test
-    public void testCreateRawAddTask() {
+    public void testCreateTask() {
         Instruction instruction = new Instruction(Instruction.Type.ADD);
         
         ParameterList params =  new ParameterList();
             params.addParameter(ParameterName.NAME,
                     ParameterValue.parseParamValue("chiong V0.1", ParameterName.NAME));
             params.addParameter(ParameterName.DATE_FROM,
-                    ParameterValue.parseParamValue("1000", ParameterName.DATE_FROM));
+                    ParameterValue.parseParamValue("20022016 1000", ParameterName.DATE_FROM));
             params.addParameter(ParameterName.DATE_TO,
-                    ParameterValue.parseParamValue("1800", ParameterName.DATE_TO));
+                    ParameterValue.parseParamValue("11032017 1800", ParameterName.DATE_TO));
         
         Command cmd = new Command(instruction, params);
         
         
-        Task task = this.dEngine_.createRawTask(cmd);
+        Task task = this.dEngine_.createTask(cmd);
         
         assertEquals(task.getTaskName(), "chiong V0.1");
-        assertEquals(task.getStartTime().getHour(), 10);
-        assertEquals(task.getEndTime().getHour(), 18);
         
+        assertEquals(task.getStartTime().getYear(), 2016);
+        assertEquals(task.getStartTime().getMonth(), Month.FEBRUARY);
+        assertEquals(task.getStartTime().getDayOfMonth(), 20);
+        assertEquals(task.getStartTime().getHour(), 10);
+        assertEquals(task.getStartTime().getMinute(), 0);
+        
+        assertEquals(task.getEndTime().getYear(), 2017);
+        assertEquals(task.getEndTime().getMonth(), Month.MARCH);
+        assertEquals(task.getEndTime().getDayOfMonth(), 11);
+        assertEquals(task.getEndTime().getHour(), 18);
+        assertEquals(task.getEndTime().getMinute(), 0);
     }
 }
