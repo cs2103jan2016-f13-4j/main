dfe9da6fa9e702e130330597add4fb2f052dfbac
Thenaesh Elango <thenaeshelango@gmail.com>
2016-03-22 18:06:16 +0800

    add setUp method in DecisionEngineTest

--- src/test/java/logic/DecisionEngineTest.java
+++ src/test/java/logic/DecisionEngineTest.java
@@ -4,8 +4,17 @@ import static org.junit.Assert.*:
 
 import org.junit.*;
 
+import java.io.IOException;
+
 /**
  * Created by thenaesh on 3/22/16.
  */
 public class DecisionEngineTest {
+    private DecisionEngine decisionEngine;
+
+    @Before
+    public void setUp() throws IOException {
+        this.decisionEngine = DecisionEngine.getInstance();
+    }
+
 }
