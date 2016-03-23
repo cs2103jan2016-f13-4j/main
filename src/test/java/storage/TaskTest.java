ab466e96eb15aa5e7c7b5d1a821532980be9693a
Chng Hui Yie <huiyiechng14@gmail.com>
2016-03-23 21:01:02 +0800

    add test for tasks with different priorities

--- src/test/java/storage/TaskTest.java
+++ src/test/java/storage/TaskTest.java
@@ -5,8 +5,12 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertSame;
 
 import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.List;
 
 import org.junit.Test;
+
+import storage.Task.Priority;
 
 /**
  * 
@@ -89,6 +93,22 @@ public class TaskTest {
                                                         // the new assigned
                                                         // value
 
+    }
+
+    @Test public void Tasks_with_different_priorities_are_ordered_correctly() {
+        Task highPriorityTask = new Task(null, "report submission", null, LocalDateTime.of(2016, 3, 8, 14, 30),
+                LocalDateTime.of(2016, 3, 8, 17, 00));
+        highPriorityTask.setPriority(Priority.HIGH);
+        Task lowPriorityTask = new Task(null, "sales team meeting", null, LocalDateTime.of(2016, 3, 8, 12, 00),
+                LocalDateTime.of(2016, 3, 8, 16, 00));
+        lowPriorityTask.setPriority(Priority.MEDIUM);
+        Storage storage = Storage.getInstance();
+        storage.save(lowPriorityTask); // save low priority task first
+        storage.save(highPriorityTask);
+        List<Task> taskList = storage.getAll();
+        Collections.sort(taskList);
+        // check that high priority task comes before low priority task
+        assertEquals(highPriorityTask, taskList.get(0));
     }
 
 }
