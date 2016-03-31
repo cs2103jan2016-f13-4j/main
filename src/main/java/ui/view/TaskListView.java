e54f5b9ecc2cb7648f794cb9edd5722c7fc617b9
tio <tio.triatmoko@outlook.com>
2016-03-31 13:06:34 +0800

    Fix bug on left and right arrow key

--- src/main/java/ui/view/TaskListView.java
+++ src/main/java/ui/view/TaskListView.java
@@ -7,6 +7,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.util.Pair;
@@ -28,7 +29,7 @@ public class TaskListView extends View {
     /**
      * Constants
      */
-    private final int MAXIMUM_DISPLAY_SIZE = 6;
+    private final int MAXIMUM_DISPLAY_SIZE = 7;
 
     /**
      * Properties
@@ -45,6 +46,7 @@ public class TaskListView extends View {
      */
     public TaskListView(List<Pair<Integer,Task>> data) {
         super(data);
+        _viewIndex = 0;
     }
 
     @Override protected void buildContent() {
@@ -105,27 +107,48 @@ public class TaskListView extends View {
         List<Pair<Integer,Task>> temp = new ArrayList<>();
         List<Pair<Integer,Task>> viewData = this.getData();
 
-        if(viewData.size() > MAXIMUM_DISPLAY_SIZE){
-            for(int i = 0; i < MAXIMUM_DISPLAY_SIZE ; i++ ){
+        int startIndex = this._viewIndex*MAXIMUM_DISPLAY_SIZE;
+        int difference  = viewData.size() - startIndex;
+        if( difference > MAXIMUM_DISPLAY_SIZE){
+            for(int i = startIndex; i < (startIndex+MAXIMUM_DISPLAY_SIZE) ; i++ ){
                 temp.add(viewData.get(i));
             }
-            return temp;
         } else {
-            return viewData;
+            for (int i = startIndex; i < viewData.size(); i++) {
+                temp.add(viewData.get(i));
+            }
         }
+
+        return temp;
 
     }
 
     @Override public Function<KeyEvent, Boolean> getKeyInputInterceptor() {
         return (event -> {
-            if (!event.getCode().isArrowKey()) {
+
+            // TODO: Handle event here
+            if(event.getCode().equals(KeyCode.UP) && canScrollUp()){
+                this._viewIndex--;
+            } else if(event.getCode().equals(KeyCode.DOWN) && canScrollDown()){
+                this._viewIndex++;
+            } else {
                 return false;
             }
 
-            // TODO: Handle event here
-            System.out.println(event.getCode());
+            this._displayList = this.constructDisplayList();
+            this._observableList.setAll( this._displayList);
             event.consume();
             return true;
         });
+    }
+
+    private boolean canScrollUp(){
+        return (this._viewIndex - 1) >= 0;
+
+    }
+    private boolean canScrollDown(){
+        List<Pair<Integer,Task>> viewData = this.getData();
+        int size = viewData.size() - (this._viewIndex+1)*MAXIMUM_DISPLAY_SIZE;
+        return size > 0;
     }
 }
