3d072fa8b923bff7f23c611339508379698aebd8
tio <tio.triatmoko@outlook.com>
2016-04-01 15:07:04 +0800

    add more undetected files from previous merge conflict

---
+++ src/test/java/helpers/IntegerationTestHelper.java
@@ -0,0 +1,38 @@
+package helpers;
+
+import javafx.scene.control.Control;
+import javafx.stage.Stage;
+import logic.Dispatcher;
+import shared.ApplicationContext;
+import skeleton.DispatcherSpec;
+
+/**
+ * Created by maianhvu on 01/04/2016.
+ */
+public class IntegerationTestHelper {
+
+    public static void startTestApplication(Stage primaryStage) {
+        // Register the main stage and flag the application as testing
+        ApplicationContext context = ApplicationContext.mainContext();
+        context.setPrimaryStage(primaryStage);
+        context.setTestingMode(true);
+
+        // Start dispatcher
+        DispatcherSpec dispatcher = Dispatcher.getInstance();
+        dispatcher.initialise();
+        dispatcher.start();
+    }
+
+    public static void shutdownTestApplication() {
+        Dispatcher.getInstance().shutdown();
+    }
+
+    public static <T extends Control> T findComponent(String query) {
+        try {
+            return (T) ApplicationContext.mainContext().getPrimaryStage().getScene()
+                    .lookup(query);
+        } catch (ClassCastException|NullPointerException e) {
+            return null;
+        }
+    }
+}
