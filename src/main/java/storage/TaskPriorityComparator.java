9d41a1ad286e01d12273a078d09b1f396c9ef191
Chng Hui Yie <huiyiechng14@gmail.com>
2016-04-02 13:55:51 +0800

    make changes to TaskPriorityComparator logic and test cases

--- src/main/java/storage/TaskPriorityComparator.java
+++ src/main/java/storage/TaskPriorityComparator.java
@@ -18,30 +18,43 @@ public class TaskPriorityComparator implements Comparator<Task> {
     }
 
     @Override public int compare(Task task1, Task task2) {
-        int priorityComparison = task1.getPriority().compareTo(task2.getPriority());
+        System.out.println("Enter compare method!");
+
+        // Priority comparison
+        int priorityComparison = task2.getPriority().compareTo(task1.getPriority());
+        System.out.println("Enter priority comparison");
         if (priorityComparison != 0) return priorityComparison;
 
         // Start time comparison
         if (task1.getStartTime() == null ^ task2.getStartTime() == null) {
+            System.out.println("Enter start time null comparison");
             if (task1.getStartTime() == null) return 1;
             if (task2.getStartTime() == null) return -1;
         }
 
         if (task1.getStartTime() != null && task2.getStartTime() != null) {
-            return task1.getStartTime().compareTo(task2.getStartTime());
+            System.out.println("Enter start time comparison");
+            int startTimeComparison = task1.getStartTime().compareTo(task2.getStartTime());
+            if (startTimeComparison != 0) return startTimeComparison;
         }
 
         // End time comparison
         if (task1.getEndTime() == null ^ task2.getEndTime() == null) {
+            System.out.println("Enter end time null comparison");
             if (task1.getEndTime() == null) return 1;
             if (task2.getEndTime() == null) return -1;
         }
 
         if (task1.getEndTime() != null && task2.getEndTime() != null) {
-            return task1.getEndTime().compareTo(task2.getEndTime());
+            System.out.println("task 1: " + task1.getEndTime().toString());
+            System.out.println("task 2: " + task2.getEndTime().toString());
+            System.out.println("Enter end time null comparison");
+            int endTimeComparison = task1.getEndTime().compareTo(task2.getEndTime());
+            if (endTimeComparison != 0) return endTimeComparison;
         }
 
-        // Creation time comparision
+        // Creation time comparison
+        System.out.println("Enter creation time null comparison");
         return task1.getCreationTime().compareTo(task2.getCreationTime());
     }
 }
